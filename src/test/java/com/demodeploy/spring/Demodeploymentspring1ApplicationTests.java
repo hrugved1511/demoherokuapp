effd9310553e923e531faef6692a7415063b701b
Hrugved <chrugved@gmal.com>
2021-05-15 11:51:14 +0530

    commit

---
+++ src/test/java/com/demodeploy/spring/Demodeploymentspring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demodeploy.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demodeploymentspring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
